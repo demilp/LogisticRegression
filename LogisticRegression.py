0e6a5e92b446fe4d4120d1adbc0a71f604db4336
demilp <demilp@gmail.com>
2017-09-07 00:11:56 -0300

    .

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -4,37 +4,29 @@ import time
 
 class LogisticRegression:
 	def __init__(self):
-		self.weights = []
+		self.weights = None
 
 	def fit(self, X, y):
 		X = np.c_[np.ones((X.shape[0], 1)), X]
-		self.weights = np.ones((X.shape[1], 1))
+		self.weights = np.ones((1, X.shape[1]))
 		self.MBGD(X, y)
 
 	def predict(self, x):
 		#s = self.weights.T.dot(x)[0]
-		s = x.dot(self.weights)
+		s = x.dot(self.weights.T)
 		return LogisticRegression.sigma(s)
 
 	def log_loss(self, X, y):
-		sum = 0
-		for i in range(X.shape[0]):
-			p = self.predict(X[i])
-			yi = y[i]
-			sum += yi * math.log(p) + (1 - yi) * math.log(1 - p)
-		return -sum / X.shape[0]
+		p = self.predict(X)
+		return -np.mean(y * np.log(p) + (1-y) * np.log(1 - p))
 
 	def gradients(self, X, y):
 		grads = np.zeros((X.shape[1], 1))
-		for j in range(self.weights.shape[0]):
-			sum = 0
-			predicted =self.predict(X)
-			dif = (predicted-y)
-			feat = X[:,j][np.newaxis]
-			s = (predicted-y)*feat
-			#for i in range(X.shape[0]):
-			#	sum += (self.predict(X[i]) - y[i][0]) * X[i][j]
-			grads[j] = sum / X.shape[0]
+		for j in range(self.weights.shape[1]):
+			#p = self.predict(X)
+			#f = X[:, j][np.newaxis].T
+			#grads[j] = np.mean((p - y) * f)
+			grads[j] = np.mean((self.predict(X)-y)*(X[:, j][np.newaxis]).T)
 		return grads
 
 	def BGD(self, X, y):
@@ -50,6 +42,8 @@ class LogisticRegression:
 			print(self.log_loss(X, y))
 
 	def MBGD(self, X, y):
+		print("loss:")
+		print(self.log_loss(X, y))
 		learning_rate = 0.5
 		n_iterations = 100
 		batch_size = 0.02
@@ -64,26 +58,28 @@ class LogisticRegression:
 		np.random.shuffle(y)
 
 		for n_iterations in range(n_iterations):
-			print("iteration number:")
-			print(n_iterations)
 			self.weights = self.weights - learning_rate * self.gradients(X[:batch_size], y[:batch_size])
-			print("weights:")
-			print(self.weights)
-			print("loss:")
-			print(self.log_loss(X, y))
+			if n_iterations % 5 == 0:
+				print("iteration number:")
+				print(n_iterations)
+				print("loss:")
+				print(self.log_loss(X, y))
 
 	def SGD(self, X, y):
 		learning_rate = 0.2
-		n_iterations = 100000
+		n_iterations = 20000
 		n_instance = np.random.randint(0, X.shape[0]+1)
 		for n_iterations in range(n_iterations):
-			print("iteration number:")
-			print(n_iterations)
+
 			self.weights = self.weights - learning_rate * self.gradients(X[n_instance:n_instance+1], y[n_instance:n_instance+1])
+			if n_iterations % 100 == 0:
+				print("iteration number:")
+				print(n_iterations)
+				print("loss:")
+				print(self.log_loss(X, y))
 		print("weights:")
 		print(self.weights)
-		print("loss:")
-		print(self.log_loss(X, y))
+
 
 	@classmethod
 	def sigma(cls, x):
